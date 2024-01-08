411257277a02d6af4575e8bef8ec8e1df46adf9b
Aira Daniella San Agustin <55384817+ai-dani@users.noreply.github.com>
2024-01-08 15:30:42 -0700

    modify exrcise 2, to create custom hook that wraps the useeffect and usestate

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -1,15 +1,9 @@
+// Custom Hook: function that uses hooks (built in or custom)
 // useEffect: persistent state
 // http://localhost:3000/isolated/exercise/02.js
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
-
-  const [name, setName] = React.useState(window.localStorage.getItem('name') || initialName)
-
-  React.useEffect(() => {
-    window.localStorage.setItem('name', name)
-  }, [name])
 // ^ dependency list is needed to avoid unnecessary re-rendering
 // Hey, React, I don't care about re-renders or anything like that. 
 // All I care about is, if the name changes, then that means the side effect 
@@ -17,9 +11,26 @@ function Greeting({initialName = ''}) {
 //an update to the state of the world to match that change in my application.
 // - Kent Dodds
 
+// this custom hook wraps a use state while extending to a use effect for storing into local storage
+const useLocalStorageWithState = (key, defaultValue='') => {
+
+  const [state, setState] = React.useState(() => window.localStorage.getItem(key) || defaultValue)
+
+  React.useEffect(() => {
+    window.localStorage.setItem(key, state)
+  }, [key, state])
+
+  return [state, setState]
+}
+
+function Greeting({initialName = ''}) {
+
+  const [name, setName] = useLocalStorageWithState(initialName);
+
 function handleChange(event) {
     setName(event.target.value)
   }
+
   return (
     <div>
       <form>
